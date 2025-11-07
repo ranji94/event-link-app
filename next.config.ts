320bf3b9caab9aa46045ea987dfb1cd261860cd8
ranji94 <jedrzej.piasecki94@gmail.com>
2025-11-07 18:31:28 +0100

    update next config

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // Umożliwia ukończenie builda mimo błędów TS (RYZYKO: możesz wdrożyć błąd do prod)
     ignoreBuildErrors: true,
-  },
-  // @ts-expect-error
-  eslint: {
-    // Opcjonalnie: nie przerywaj builda przez błędy ESLint
-    ignoreDuringBuilds: true,
   },
 };
 
